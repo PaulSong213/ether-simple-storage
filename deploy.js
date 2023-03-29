55c6a3681ceec95365fa03e242608ac1d73d356f
John Paul Songalia <songaliajohnpaul@gmail.com>
2023-03-30 06:54:21 +0800

    put private keys in .env

--- deploy.js
+++ deploy.js
@@ -1,15 +1,11 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
+
 //https://stackoverflow.com/questions/74197765/i-am-experiencing-a-could-not-detect-network-event-nonetwork-code-network-e
 async function main() {
-	//http://127.0.0.1:8545
-	const provider = new ethers.providers.JsonRpcProvider(
-		"http://127.0.0.1:8545"
-	);
-	const wallet = new ethers.Wallet(
-		"0xccf64d6390a0938b30cf8d9dbded876cf5960bb5080625ce17535ae26935c092",
-		provider
-	);
+	const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+	const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 	const abi = fs.readFileSync(
 		"./SimpleStorage_sol_SimpleStorage.abi",
 		"utf8"
